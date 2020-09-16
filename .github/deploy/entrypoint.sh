1d0956ba62e32a2696b6008e4c8688643ee4ce68
Juho Friman <juho.friman@solita.fi>
2020-09-16 16:48:23 +0300

    We have encountered some problems with slack webhook deploy

--- .github/deploy/entrypoint.sh
+++ .github/deploy/entrypoint.sh
@@ -5,4 +5,7 @@ set -e
 message=`cat the-program.txt`
 
 curl -X POST -H 'Content-type: application/json' --data "{\"text\": \"$GITHUB_SHA $GITHUB_REF\"}" $SLACK_WEBHOOK
+
+sleep 3
+
 curl -X POST -H 'Content-type: application/json' --data "{\"text\": \"$message\"}" $SLACK_WEBHOOK
